050d6c08be8c06895826d13917ccb51a8f00d8ef
tototjc <53078240+tototjc@users.noreply.github.com>
2025-03-23 11:09:15 +0800

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import js from '@eslint/js'
 import ts from 'typescript-eslint'
 import astro from 'eslint-plugin-astro'
-import configPrettier from 'eslint-config-prettier/flat'
+import prettierConfig from 'eslint-config-prettier/flat'
 import globals from 'globals'
 
 import { defineConfig } from 'eslint/config'
@@ -9,7 +9,7 @@ import { includeIgnoreFile } from '@eslint/compat'
 import { fileURLToPath } from 'node:url'
 
 export default defineConfig([
-  includeIgnoreFile(fileURLToPath(import.meta.resolve('./.gitignore'))),
+  includeIgnoreFile(fileURLToPath(await import.meta.resolve('./.gitignore'))),
   js.configs.recommended,
   ts.configs.recommended,
   astro.configs.recommended,
@@ -27,5 +27,5 @@ export default defineConfig([
       },
     },
   },
-  configPrettier,
+  prettierConfig,
 ])
