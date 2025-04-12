dbba07edc3243eb247d85e40ceb807321f3b8675
tototjc <53078240+tototjc@users.noreply.github.com>
2025-04-12 23:33:19 +0800

    feat: add Open Graph image generation and html meta tag

---
+++ src/pages/[...slug]/og.png.ts
@@ -0,0 +1,34 @@
+import type { APIRoute, GetStaticPaths, InferGetStaticPropsType, InferGetStaticParamsType } from 'astro'
+import { experimental_AstroContainer } from 'astro/container'
+import { title as siteTitle } from 'virtual:config'
+import sharp from 'sharp'
+
+import { contents } from '@/utils/getContents'
+
+import OgImg from '@/components/OgImg.astro'
+
+const container = await experimental_AstroContainer.create()
+
+export const getStaticPaths = (() =>
+  Array.fromAsync(contents, async entry => ({
+    params: { slug: entry.id },
+    props: {
+      img: await sharp(
+        new TextEncoder().encode(
+          await container.renderToString(OgImg, { props: { title: entry.data.title, subtitle: siteTitle } }),
+        ),
+      )
+        .toFormat('png')
+        .toBuffer(),
+    },
+  }))) satisfies GetStaticPaths
+
+type Props = InferGetStaticPropsType<typeof getStaticPaths>
+type Params = InferGetStaticParamsType<typeof getStaticPaths>
+
+export const GET: APIRoute<Props, Params> = ({ props }) =>
+  new Response(props.img, {
+    headers: {
+      'Content-Type': 'image/png',
+    },
+  })
