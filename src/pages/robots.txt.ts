2a0f1df4bf74847a9a8389f09d54294a247790d6
tototjc <53078240+tototjc@users.noreply.github.com>
2025-08-08 13:43:02 +0800

    refactor: restructure robots.txt generation for improved readability

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,12 +1,30 @@
 import type { APIRoute } from 'astro'
 
+type RobotsPolicy = {
+  userAgent: string[]
+  allow?: string[]
+  disallow?: string[]
+}[]
+
+const policy: RobotsPolicy = [
+  {
+    userAgent: ['*'],
+    allow: ['/'],
+    disallow: ['/admin'],
+  },
+]
+
 export const GET: APIRoute = ({ site }) => {
-  const robotsTxt = `
-    User-agent: *
-    Allow: /
-    Disallow: /admin
-    Sitemap: ${URL.parse('/sitemap-index.xml', site)?.href}
-  `.trim()
+  const robotsTxt = [
+    ...policy.map(({ userAgent, allow, disallow }) =>
+      [
+        ...userAgent.map(i => `User-agent: ${i}`),
+        ...(allow ?? []).map(i => `Allow: ${i}`),
+        ...(disallow ?? []).map(i => `Disallow: ${i}`),
+      ].join('\n'),
+    ),
+    `Sitemap: ${URL.parse('/sitemap-index.xml', site)?.href}`,
+  ].join('\n\n')
   return new Response(robotsTxt, {
     headers: {
       'Content-Type': 'text/plain; charset=utf-8',
