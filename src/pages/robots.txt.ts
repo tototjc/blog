7b19ed37cb6a657b8fe4a7c468f3c576cfd53a5d
tototjc <53078240+tototjc@users.noreply.github.com>
2025-08-09 12:08:18 +0800

    fix: update homepage link in 404 page and simplify robots.txt response handling

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -14,20 +14,21 @@ const policy: RobotsPolicy = [
   },
 ]
 
-export const GET: APIRoute = ({ site }) => {
-  const robotsTxt = [
-    ...policy.map(({ userAgent, allow, disallow }) =>
-      [
-        ...userAgent.map(i => `User-agent: ${i}`),
-        ...(allow ?? []).map(i => `Allow: ${i}`),
-        ...(disallow ?? []).map(i => `Disallow: ${i}`),
-      ].join('\n'),
-    ),
-    `Sitemap: ${URL.parse('/sitemap-index.xml', site)?.href}`,
-  ].join('\n\n')
-  return new Response(robotsTxt, {
-    headers: {
-      'Content-Type': 'text/plain; charset=utf-8',
+export const GET: APIRoute = ({ site }) =>
+  new Response(
+    [
+      ...policy.map(({ userAgent, allow, disallow }) =>
+        [
+          ...userAgent.map(i => `User-agent: ${i}`),
+          ...(allow ?? []).map(i => `Allow: ${i}`),
+          ...(disallow ?? []).map(i => `Disallow: ${i}`),
+        ].join('\n'),
+      ),
+      `Sitemap: ${URL.parse('/sitemap-index.xml', site)?.href}`,
+    ].join('\n\n'),
+    {
+      headers: {
+        'Content-Type': 'text/plain; charset=utf-8',
+      },
     },
-  })
-}
+  )
